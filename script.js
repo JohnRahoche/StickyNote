3cbc2444811cde06550893abcb5201ed675d6f1b
JohnRahoche <johnrahoche@gmail.com>
2025-03-11 15:27:46 -0400

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-var zIndexCount = 0;
-function createStickyNote(){
-    var newNote = document.createElement("div");
-    // newNote.contentEditable = true;
-    //Styling the note
-    newNote.className="stickyNote";
-    newNote.style.zIndex = zIndexCount;
-    zIndexCount++;
-    newNote.style.transform = 'rotate('+Math.random(0,45)+"deg)";
+var editModeOn = false;
+function toggleEdit() {
+  editModeOn = !editModeOn;
+  document.getElementById("editButton").classList.toggle("selected");
+}
+function createStickyNote() {
+  var newNote = document.createElement("div");
+  // newNote.contentEditable = true;
 
-    
-    document.body.appendChild(newNote);
-    const notes = document.getElementsByClassName("stickyNote");
+  //Styling the note
+  newNote.className = "stickyNote";
+  newNote.style.transform = "rotate(" + Math.random(0, 45) + "deg)";
 
-    Array.from(notes).forEach(dragStickyNote);
-    
-  }
-  
+  document.body.appendChild(newNote);
   const notes = document.getElementsByClassName("stickyNote");
-    
-    Array.from(notes).forEach(dragStickyNote);
 
-    function dragStickyNote(element){
-      var pos1=0, pos2=0, pos3=0, pos4=0;
-      element.onmousedown = dragMouseDown;
-      
-  
-    function dragMouseDown(e) {
+  Array.from(notes).forEach(dragStickyNote);
+}
+
+const notes = document.getElementsByClassName("stickyNote");
+
+Array.from(notes).forEach(dragStickyNote);
+
+function dragStickyNote(element) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  element.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    if (editModeOn === true) {
+      element.contentEditable = true;
+    } else {
       e = e || window.event;
       e.preventDefault();
       pos3 = e.clientX;
       pos4 = e.clientY;
       element.classList.toggle("dragging");
-      
-  //     When user unclicks, it stops dragging the sticky
-      document.onmouseup = ()=>{
+
+      //     When user unclicks, it stops dragging the sticky
+      document.onmouseup = () => {
         document.onmouseup = null;
         document.onmousemove = null;
 
@@ -41,36 +47,48 @@ function createStickyNote(){
         element.classList.toggle("dragging");
 
         // This is logic to ensure that the sticky note does not leave the screen (with some clipping allowed)
-        if(element.offsetTop - pos2 < document.getElementById("buttonContainer").clientHeight){
-          console.log("Reset Top")
-          element.style.top= "50%";
+        if (
+          element.offsetTop - pos2 <
+          document.getElementById("buttonContainer").clientHeight
+        ) {
+          console.log("Reset Top");
+          element.style.top = "50%";
         }
-        if(element.offsetLeft - pos1 < 25){
-          element.style.left= "50%";
+        if (element.offsetLeft - pos1 < 25) {
+          element.style.left = "50%";
         }
-      }
-      
+      };
+
       document.onmousemove = draggingLogic;
     }
-  
-    function draggingLogic(e){
+
+    function draggingLogic(e) {
       e = e || window.event;
       e.preventDefault();
       pos1 = pos3 - e.clientX;
       pos2 = pos4 - e.clientY;
       pos3 = e.clientX;
       pos4 = e.clientY;
-      
-      element.style.top = (element.offsetTop - pos2) + "px";
-      element.style.left = (element.offsetLeft - pos1) + "px";
+
+      element.style.top = element.offsetTop - pos2 + "px";
+      element.style.left = element.offsetLeft - pos1 + "px";
     }
   }
-  
-  
+}
+function resetArea() {
+  let result = confirm("Are you sure you to delete all sticky notes?");
 
-function resetArea(){
-  const notes = document.getElementsByClassName("stickyNote");
-  Array.from(notes).forEach((note)=>{
-    note.remove();
-  });
+  if (result) {
+    document.getElementById("screenWipe").style.animation =
+      "screenfire 3s ease-in-out forwards";
+    setTimeout(() => {
+      const notes = document.getElementsByClassName("stickyNote");
+      Array.from(notes).forEach((note) => {
+        note.remove();
+      });
+    }, 1500);
+    setTimeout(() => {
+      document.getElementById("screenWipe").style.animation = "";
+    }, 3000);
+  }
 }
