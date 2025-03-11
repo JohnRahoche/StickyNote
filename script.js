241656328e7d59d02045ac8290491779a34687b5
JohnRahoche <johnrahoche@gmail.com>
2025-03-11 12:03:31 -0400

    new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,76 @@
+var zIndexCount = 0;
+function createStickyNote(){
+    var newNote = document.createElement("div");
+    // newNote.contentEditable = true;
+    //Styling the note
+    newNote.className="stickyNote";
+    newNote.style.zIndex = zIndexCount;
+    zIndexCount++;
+    newNote.style.transform = 'rotate('+Math.random(0,45)+"deg)";
+
+    
+    document.body.appendChild(newNote);
+    const notes = document.getElementsByClassName("stickyNote");
+
+    Array.from(notes).forEach(dragStickyNote);
+    
+  }
+  
+  const notes = document.getElementsByClassName("stickyNote");
+    
+    Array.from(notes).forEach(dragStickyNote);
+
+    function dragStickyNote(element){
+      var pos1=0, pos2=0, pos3=0, pos4=0;
+      element.onmousedown = dragMouseDown;
+      
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      element.classList.toggle("dragging");
+      
+  //     When user unclicks, it stops dragging the sticky
+      document.onmouseup = ()=>{
+        document.onmouseup = null;
+        document.onmousemove = null;
+
+        // This lets us style while dragging
+        element.classList.toggle("dragging");
+
+        // This is logic to ensure that the sticky note does not leave the screen (with some clipping allowed)
+        if(element.offsetTop - pos2 < document.getElementById("buttonContainer").clientHeight){
+          console.log("Reset Top")
+          element.style.top= "50%";
+        }
+        if(element.offsetLeft - pos1 < 25){
+          element.style.left= "50%";
+        }
+      }
+      
+      document.onmousemove = draggingLogic;
+    }
+  
+    function draggingLogic(e){
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      
+      element.style.top = (element.offsetTop - pos2) + "px";
+      element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+  }
+  
+  
+
+function resetArea(){
+  const notes = document.getElementsByClassName("stickyNote");
+  Array.from(notes).forEach((note)=>{
+    note.remove();
+  });
+}
